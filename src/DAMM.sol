fd9697c3a1fced90f97131948a01ed9c327c4b60
Rati Montreewat <ratixoxo@gmail.com>
2024-05-24 18:51:08 +0700

    Build (./src): Adding dependencies for DAMM

--- src/DAMM.sol
+++ src/DAMM.sol
@@ -1,2 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {Currency} from "v4-core/types/Currency.sol";
+import {CurrencySettleTake} from "v4-core/libraries/CurrencySettleTake.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
+import {BaseHook} from "./univ4/BaseHook.sol";
+
+contract DAMM {
+// contract DAMM is BaseHook {
+
+
+    using CurrencySettleTake for Currency;
+
+    error AddLiquidityThroughHook();
+
+    struct CallbackData {
+        uint256 amountEach;
+        Currency currency0;
+        Currency currency1;
+        address sender;
+    }
+
+    constructor(IPoolManager poolManager){}
+    // constructor(IPoolManager poolManager) BaseHook(poolManager) {}
+
+}
